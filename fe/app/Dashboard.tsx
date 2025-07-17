2b74ba4f47209190478860192418ed700a9175ea
CollapsedNebula <3957yi@naver.com>
2025-07-17 11:04:05 +0900

    added auto signin & signout feature

--- fe/app/Dashboard.tsx
+++ fe/app/Dashboard.tsx
@@ -3,6 +3,9 @@ import { useRouter } from 'expo-router';
 import { StyleSheet, View } from "react-native";
 import { ThemedText } from "../components/ThemedText";
 import { useCalendar } from "./CalendarContext";
+import { ThemedButton } from "@/components/ThemedButton";
+import axiosInstance from "@/api/AxiosInstance";
+import * as SecureStore from 'expo-secure-store';
 
 export default function Dashboard() {
   const { year, month } = useCalendar();
@@ -10,6 +13,25 @@ export default function Dashboard() {
 
   const handleDayPress = (dateTag: string) => {
     router.push(`/date/${dateTag}`);
+  };
+
+  const handleSignOut = async() => {
+    console.log("로그아웃 시도");
+    try {
+      await axiosInstance.post("/auth/logout");
+
+      await SecureStore.deleteItemAsync('token');
+      console.log("토큰 삭제 성공");
+
+      delete axiosInstance.defaults.headers.common['Authorization'];
+      console.log("Authorization 헤더 삭제 성공");
+
+      console.log("로그아웃 성공");
+      router.replace("/SignIn");
+    } catch (error) {
+      console.error("로그아웃 실패:", error.response ? error.response.data : error.message);
+      alert("로그아웃에 실패했습니다.");
+    }
   };
 
   return (
@@ -22,6 +44,7 @@ export default function Dashboard() {
         }}
       >대시보드</ThemedText>
       <Calendar year={year} month={month} onDayPress={handleDayPress} />
+      <ThemedButton title="로그아웃" onPress={handleSignOut} />
     </View>
   );
 }
