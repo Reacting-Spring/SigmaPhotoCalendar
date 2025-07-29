ff5cfc1ad7e192f1e67241aa21f6f254102989e0
김정우 <3957ki@gmail.com>
2025-07-29 20:34:45 +0900

    로그인 수정 및 axios 인터셉터로 토큰관리 추가

--- react/src/pages/SignIn.tsx
+++ react/src/pages/SignIn.tsx
@@ -16,22 +16,17 @@ export default function SignIn() {
     console.log("로그인 시도:", { user_id, password }, `저장 여부: ${saveSignIn}`);
     try {
       const response = await axiosInstance.post("/auth/login", { user_id, password });
-      console.log("로그인 성공:", response.data);
 
-      const token = response.data;
-
-      if (token) {
-        if (saveSignIn) {
-          await useAuthStore.getState().setAccessToken(token);
-          console.log("토큰 저장 성공");
-        }
-
-        axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-      } else {
-        console.error("로그인 실패: 토큰이 없습니다.");
+      if (response.status !== 200) {
+        alert("로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.");
+        return;
       }
 
-      return navigate("/Dashboard");
+      const { access_token } = response.data;
+      useAuthStore.getState().setAccessToken(access_token);
+      console.log("토큰 저장 성공:", access_token);
+      axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+      navigate("/Dashboard");
     } catch (error) {
       console.error("로그인 실패:", error);
       alert("로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.");
@@ -46,19 +41,17 @@ export default function SignIn() {
     <>
       <div>아이디</div>
       <ThemedInput
-        onChangeText={setId}
+        onChange={(e) => setId(e.target.value)}
         value={user_id}
         placeholder="아이디를 입력하세요"
-        keyboardType="default"
         style={{ marginBottom: 24 }}
       />
       <div>비밀번호</div>
       <ThemedInput
-        onChangeText={setPassword}
+        onChange={(e) => setPassword(e.target.value)}
         value={password}
         placeholder="비밀번호를 입력하세요"
-        secureTextEntry
-        keyboardType="default"
+        type="password"
       />
       <Link to="/SignUp" style={{ padding: 8 }}>
         <div style={{ color: "#4B72FA", marginTop: 16, fontSize: 16 }}>계정이 없다면 회원가입</div>
